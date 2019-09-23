1924e6a5c3d6160374200165351107d33928c4a4
MattMan569 <matthewpolsom@hotmail.com>
2019-09-23 17:44:37 -0600

    added parseIngredients()

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -16,7 +16,7 @@ export default class Recipe {
             this.author = recipe.source;
             this.image = recipe.image;
             this.url = recipe.url;
-            this.ingredients = recipe.ingredients;
+            this.ingredients = recipe.ingredients.map((ingredient) => ingredient.text);
         } catch (error) {
             console.error(error);
         }
@@ -31,5 +31,63 @@ export default class Recipe {
 
     calcServings() {
         this.servings = 4;
+    }
+
+    parseIngredients() {
+        // NOTE: always put plural first
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds', 'pound', 'grams', 'gram'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'lbs', 'lbs', 'g', 'g'];
+
+        const newIngredients = this.ingredients.map(el => {
+            // 1. Uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+
+            // 2. Remove parenthesis
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, '');
+
+            // 3. Parse ingredients into count, unit, and ingredient
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el => unitsShort.includes(el));
+
+            let objIng;
+
+            if (unitIndex > -1) {
+                // There is a unit
+                const arrCount = arrIng.slice(0, unitIndex); // Eg. 4 1/2 cups => [4, 1/2]
+                
+                let count;
+                if (arrCount.length === 1) {
+                    count = eval(arrIng[0].replace('-', '+'));
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+')) // Eg. "4+1/2" => 4.5
+                }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
+                };
+            } else if (parseInt(arrIng[0], 10)) {
+                // There is no unit, but there is a number
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                };
+            } else if (unitIndex === -1) {
+                // There is no unit
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                };
+            }
+
+            return objIng;
+        });
+        this.ingredients = newIngredients;
     }
 }
